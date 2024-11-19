f83669f01b19ad75fe34dd0c7a8cc0bfa772e0c6
qtiankong <167791815+qtiankong@users.noreply.github.com>
2024-11-19 14:19:06 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -2,11 +2,7 @@ const pageConfig = {
   // Title for your status page
   title: "CF-WEB Status Page",
   // Links shown at the header of your status page, could set `highlight` to `true`
-  links: [
-    { link: 'https://github.com/lyc8503', label: 'GitHub' },
-    { link: 'https://blog.lyc8503.net/', label: 'Blog' },
-    { link: 'mailto:me@lyc8503.net', label: 'Email Me', highlight: true },
-  ],
+
 }
 
 const workerConfig = {
